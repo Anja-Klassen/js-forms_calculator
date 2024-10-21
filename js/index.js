08e76d6022413e62a40beaab53f0a7a08cf39ee2
Anja-Klassen <anja.klassen@posteo.de>
2024-10-21 11:19:08 +0200

    add solution and comments

--- js/index.js
+++ js/index.js
@@ -21,12 +21,32 @@ function divide(a, b) {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
+// variable result wird erstellt, aber noch kein Wert zugewiesen -> fungiert als Platzhalter; kann später genutzt werden, um das Ergebnis der Berechnung zu speochern
   let result;
 
   // --v-- write your code here --v--
+//zunächst Daten aus dem Formular verfügbar machen/anwählen
+const formData= new FormData(event.target)
+const data = Object.fromEntries(formData);
+//converting stringvalue input to numbers
+const numberA = Number(data.numberA);
+const numberB = Number(data.numberB);
+const operator = data.operator;
 
+//check which operator is selected and perform the corresponding mathematical operation (use the value here to identify)
+if (operator === "addition"){
+ result = add(numberA, numberB);
+}
+if (operator === "subtraction"){
+  result = subtract(numberA, numberB);
+}
+if (operator === "multiplication"){
+  result = multiply(numberA, numberB);
+}
+if (operator === "division"){
+  result = divide(numberA, numberB);
+}
   // --^-- write your code here --^--
-
+// updates the text content of the resultOutput element -Y displays the value stored in result
   resultOutput.textContent = result;
 });
